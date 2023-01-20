d2e81217d56d861ebd8a59c31ce2e60a701c481c
zaq <zaqrashyy7@gmail.com>
2023-01-20 12:55:16 +0100

    First Push To replace default files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Hero from "./Main/Hero";
+import Nav from "./common/Nav.js";
+import Body from "./Main/body";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <>
+        <Nav />
+        <Hero />
+        <Body/>
+        </>
   );
 }
 
